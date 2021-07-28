81dce25912ac4ac0c1a1d3b63ae8611084d9f899
Relue1729 <73345451+Relue1729@users.noreply.github.com>
2021-07-28 19:48:24 +0300

    Initial commit

---
+++ WebAPISampleProject/Controllers/RGDialogsClientsController.cs
@@ -0,0 +1,36 @@
+﻿using iMessengerCoreAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace WebAPISampleProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RGDialogsClientsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Guid> Get([FromQuery] Guid[] ClientIDs)
+        {
+            if (ClientIDs.Length == 0)
+            {
+                base.Response.StatusCode = (int)HttpStatusCode.NoContent;
+                return null;
+            }
+
+            var dialogsGroupedByID = new RGDialogsClients().Init().GroupBy(x => x.IDRGDialog);
+
+            var dialogsContainingAllClients = dialogsGroupedByID.Where(x => !ClientIDs.Except(x.Select(y => y.IDClient))
+                                                                                      .Any())
+                                                                .Select(x => x.Key);
+            if (dialogsContainingAllClients.Count() == 0)
+            {
+                return new Guid[] { Guid.Empty }; //Если диалог со всеми клиентами не найден, то возвращается пустой GUID
+            }
+
+            return dialogsContainingAllClients;
+        }
+    }
+}
